46e072b790db00be6a504980eaeddbd3c0795117
idong00 <idong00@naver.com>
2020-04-08 19:38:50 +0900

    add color to shaders

--- DirectX11/Graphics/Vertex.h
+++ DirectX11/Graphics/Vertex.h
@@ -5,10 +5,12 @@
 struct Vertex
 {
 	DirectX::XMFLOAT2 pos;
+	DirectX::XMFLOAT3 color;
 
 	Vertex() {}
-	Vertex(float x, float y) :
-		pos(x, y)
+	Vertex(float x, float y, float r, float g, float b) :
+		pos(x, y),
+		color(r, g, b)
 	{
 	}
 };
