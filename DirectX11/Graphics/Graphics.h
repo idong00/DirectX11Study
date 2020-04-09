1eefae99965b6d20f3b87ad54faed1fc8ec44bcf
idong00 <idong00@naver.com>
2020-04-09 20:36:52 +0900

    add constant buffer template class

--- DirectX11/Graphics/Graphics.h
+++ DirectX11/Graphics/Graphics.h
@@ -8,7 +8,8 @@
 #include <WICTextureLoader.h>
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
-#include "ConstantBufferTypes.h"
+#include "ConstantBuffer.h"
+
 
 class Graphics {
 public:
@@ -30,7 +31,7 @@ private:
 
 	VertexShader vertexshader;
 	PixelShader pixelshader;
-	Microsoft::WRL::ComPtr<ID3D11Buffer> constantBuffer;
+	ConstantBuffer<CB_VS_vertexshader> constantBuffer;
 
 	VertexBuffer<Vertex> vertexBuffer;
 	IndexBuffer indicesBuffer;
