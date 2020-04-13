ee7196ac73e7ba6ff16453994455adc30aee7bbe
idong00 <idong00@naver.com>
2020-04-13 20:38:25 +0900

    set up ASSIMP to load model

--- DirectX11/Graphics/AdapterReader.h
+++ DirectX11/Graphics/AdapterReader.h
@@ -2,9 +2,6 @@
 
 #include "../ErrorLogger.h"
 #include <d3d11.h>
-#pragma comment(lib, "d3d11.lib")
-#pragma comment(lib, "DirectXTK.lib")
-#pragma comment(lib, "DXGI.lib")
 #include <wrl/client.h>
 #include <vector> 
 
